e7f3bca025ff7f9d7baffc8db57e26dafb1af8d7
valen <valentin.hagnere@hotmail.fr>
2019-03-29 15:26:33 +0100

    test json decode

--- controleurs/appGetData.php
+++ controleurs/appGetData.php
@@ -5,6 +5,7 @@ require_once '../includes/class.pdogsb.inc.php';
 $pdo = PdoGsb::getPdoGsb();
 $json = file_get_contents('php://input');
 $lesFraisHF = json_decode($json, true);
+
 //$lesFrais = array($typeVehicule => $km, "ETP" => $etp, "NUI" => $nui, "REP" => $rep);
 //$pdo->majFraisForfait($id, $mois, $lesFrais);
-print_r($lesFraisHF);
+echo lesFraisHF['etape'];
