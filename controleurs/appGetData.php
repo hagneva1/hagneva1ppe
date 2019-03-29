7cc4e9d905951be06a86c2fe56f261c027216877
valen <valentin.hagnere@hotmail.fr>
2019-03-29 14:06:33 +0100

    testjson

--- controleurs/appGetData.php
+++ controleurs/appGetData.php
@@ -11,6 +11,7 @@ $etp = filter_input(INPUT_POST, 'ETP', FILTER_SANITIZE_STRING);
 $nui = filter_input(INPUT_POST, 'NUI', FILTER_SANITIZE_STRING);
 $rep = filter_input(INPUT_POST, 'REP', FILTER_SANITIZE_STRING);
 $lesFraisHF = filter_input(INPUT_POST, 'lesFraisHF', FILTER_DEFAULT, FILTER_FORCE_ARRAY);
+$lesFraisHF = json_decode($lesFraisHF);
 $lesFrais = array($typeVehicule => $km, "ETP" => $etp, "NUI" => $nui, "REP" => $rep);
 //$pdo->majFraisForfait($id, $mois, $lesFrais);
 print_r($lesFraisHF);
