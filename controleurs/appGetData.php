267f10658e3306e9b60301775e7944e665d2e5e2
valen <valentin.hagnere@hotmail.fr>
2019-03-29 18:11:13 +0100

    issue on date solved

--- controleurs/appGetData.php
+++ controleurs/appGetData.php
@@ -10,9 +10,9 @@ $lesFrais = array($lesFraisHF[0]['typeVehicule'] => $lesFraisHF[0]['km'],
     "ETP" => $lesFraisHF[0]['etape'], "NUI" => $lesFraisHF[0]['nuitee'],
     "REP" => $lesFraisHF[0]['repas']);
 if ($lesFraisHF[0]['mois'] < 10) {
-    $mois = $lesFraisHF[0]['annee']+'0'+$lesFraisHF[0]['mois'];
+    $mois = $lesFraisHF[0]['annee'].'0'.$lesFraisHF[0]['mois'];
 } else {
-    $mois = $lesFraisHF[0]['annee']+$lesFraisHF[0]['mois'];
+    $mois = $lesFraisHF[0]['annee'].$lesFraisHF[0]['mois'];
 }
 echo $id.' '.$mois.' '.$lesFraisHF[0]['etape'];
 $pdo->majFraisForfait($id, $mois, $lesFrais);
