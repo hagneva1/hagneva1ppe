b0b72711d77af87728fc2c71f635d3294ac856f2
valen <valentin.hagnere@hotmail.fr>
2019-03-29 15:11:48 +0100

    test receive json

--- controleurs/appGetData.php
+++ controleurs/appGetData.php
@@ -3,15 +3,8 @@
 require_once '../includes/fct.inc.php';
 require_once '../includes/class.pdogsb.inc.php';
 $pdo = PdoGsb::getPdoGsb();
-$id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_STRING);
-$mois = filter_input(INPUT_POST, 'mois', FILTER_SANITIZE_STRING);
-$typeVehicule = filter_input(INPUT_POST, 'typeVeh', FILTER_SANITIZE_STRING);
-$km = filter_input(INPUT_POST, 'km', FILTER_SANITIZE_STRING);
-$etp = filter_input(INPUT_POST, 'ETP', FILTER_SANITIZE_STRING);
-$nui = filter_input(INPUT_POST, 'NUI', FILTER_SANITIZE_STRING);
-$rep = filter_input(INPUT_POST, 'REP', FILTER_SANITIZE_STRING);
-$json = filter_input(INPUT_POST, 'lesFraisHF', FILTER_SANITIZE_STRING);
-$lesFraisHF = json_decode($json, true);
-$lesFrais = array($typeVehicule => $km, "ETP" => $etp, "NUI" => $nui, "REP" => $rep);
+$json = file_get_contents('php://input');
+$lesFraisHF = json_decode($json);
+//$lesFrais = array($typeVehicule => $km, "ETP" => $etp, "NUI" => $nui, "REP" => $rep);
 //$pdo->majFraisForfait($id, $mois, $lesFrais);
 print_r($lesFraisHF);
